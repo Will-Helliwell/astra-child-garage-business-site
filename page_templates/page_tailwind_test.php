e9f12cd5ae2ca3481194b8c20e52c75eb2c1af4c
Will-Helliwell <wj.helliwell@gmail.com>
2024-07-19 14:23:31 +0100

    Custom page content displayed within middle of template.

--- page_templates/page_tailwind_test.php
+++ page_templates/page_tailwind_test.php
@@ -11,9 +11,16 @@
 <div class="bg-blue-500 text-white p-4">
     <h1 class="text-3xl">Hello, Tailwind!</h1>
 </div>
+<?php echo do_shortcode('[testimonial_slider]'); ?>
+
 <div class="custom-page-content">
-    <!-- Insert shortcode here -->
-    <?php echo do_shortcode('[testimonial_slider]'); ?>
+    <?php
+    if (have_posts()) :
+        while (have_posts()) : the_post();
+            the_content();
+        endwhile;
+    endif;
+    ?>
 </div>
 
 <?php get_footer(); ?>
