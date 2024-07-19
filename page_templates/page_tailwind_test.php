dbd0daf7d9ed2128e33cd8058ca93c7826aa62c3
Will-Helliwell <wj.helliwell@gmail.com>
2024-07-19 13:54:16 +0100

    Tailwind working on sample page.

---
+++ page_templates/page_tailwind_test.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Template Name: Tailwind Test
+ * description: >-
+  Page template for testing tailwind
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="bg-blue-500 text-white p-4">
+    <h1 class="text-3xl">Hello, Tailwind!</h1>
+</div>
+
+<?php get_footer(); ?>
