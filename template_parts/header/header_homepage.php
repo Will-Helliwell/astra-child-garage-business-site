7f0c87eed57831ce11cc792e7d4b036a5855675c
Will-Helliwell <wj.helliwell@gmail.com>
2024-07-23 10:05:20 +0100

    Exctract menu to separate template part.

--- template_parts/header/header_homepage.php
+++ template_parts/header/header_homepage.php
@@ -1,21 +1,6 @@
 <div class="header-homepage bg-cover bg-center bg-no-repeat p-6 text-white text-center h-screen"
      style="background-image: url('<?php echo esc_url(wp_get_attachment_url(24)); ?>');">
-    <div class="top-row flex justify-between items-center">
-        <div class="site-logo flex-1">
-            <?php if (has_custom_logo()) {
-                the_custom_logo();
-            } else { ?>
-                <a href="<?php echo home_url(); ?>" class="text-2xl font-bold"><?php bloginfo('name'); ?></a>
-            <?php } ?>
-        </div>
-        <nav class="navigation-menu flex-2">
-            <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'flex space-x-4')); ?>
-        </nav>
-        <div class="header-homepage-buttons flex-1 flex justify-around">
-            <a href="#" class="header-homepage-button bg-black text-white py-2 px-4 rounded hover:bg-gray-700">Button 1</a>
-            <a href="#" class="header-homepage-button bg-black text-white py-2 px-4 rounded hover:bg-gray-700">Button 2</a>
-        </div>
-    </div>
+    <?php get_template_part('template_parts/header/menu', 'content') ?>
     <div class="bottom-row mt-10">
         <h1 class="title text-4xl font-bold mb-4"><?php the_title(); ?></h1>
         <h2 class="subtitle text-2xl mb-4">Your Subtitle Here</h2>
---
+++ template_parts/header/menu.php
@@ -0,0 +1,16 @@
+<div class="top-row flex justify-between items-center">
+    <div class="site-logo flex-1">
+        <?php if (has_custom_logo()) {
+            the_custom_logo();
+        } else { ?>
+            <a href="<?php echo home_url(); ?>" class="text-2xl font-bold"><?php bloginfo('name'); ?></a>
+        <?php } ?>
+    </div>
+    <nav class="navigation-menu flex-2">
+        <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'flex space-x-4')); ?>
+    </nav>
+    <div class="header-homepage-buttons flex-1 flex justify-around">
+        <a href="#" class="header-homepage-button bg-black text-white py-2 px-4 rounded hover:bg-gray-700">Button 1</a>
+        <a href="#" class="header-homepage-button bg-black text-white py-2 px-4 rounded hover:bg-gray-700">Button 2</a>
+    </div>
+</div>
