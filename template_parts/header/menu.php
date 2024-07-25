97e6dfc912de9c38dfdabb7b000e0a7ddebb2809
Will-Helliwell <wj.helliwell@gmail.com>
2024-07-25 19:20:04 +0800

    Apply custom font size to primary menu.

--- template_parts/header/menu.php
+++ template_parts/header/menu.php
@@ -6,10 +6,10 @@
             <a href="<?php echo home_url(); ?>" class="text-2xl font-bold"><?php bloginfo('name'); ?></a>
         <?php } ?>
     </div>
-    <nav class="navigation-menu flex-2">
+    <nav class="menu-primary-custom flex-2">
         <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'flex space-x-4')); ?>
     </nav>
-    <div class="header-homepage-buttons flex-1 flex justify-around">
+    <div class="header-homepage-buttons flex-2 flex justify-around">
         <a href="#" class="header-homepage-button bg-black text-white py-2 px-4 rounded hover:bg-gray-700">Button 1</a>
         <a href="#" class="header-homepage-button bg-black text-white py-2 px-4 rounded hover:bg-gray-700">Button 2</a>
     </div>
