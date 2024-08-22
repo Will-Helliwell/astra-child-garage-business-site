2a155177f301387a6429d5777c48fd28370e95e7
Will-Helliwell <wj.helliwell@gmail.com>
2024-08-22 16:09:50 +0800

    Add different images for different main pages.

--- template_parts/header/header_main_page.php
+++ template_parts/header/header_main_page.php
@@ -1,12 +1,16 @@
 <?php
 switch ($pagename) {
-    case 'Services':
-        $background_image_id = 1;
-        $subtitle_text = 'xxx';
+    case 'services':
+        $background_image_id = 54;
+        $subtitle_text = 'Comprehensive Solutions for Your Needs in Los Angeles, Orange County and Nationwide';
         break;
     case 'who-we-are':
         $background_image_id = 50;
         $subtitle_text = 'Discover The Story Behind Grand Slam Garage Sales';
+        break;
+    case 'how-it-works':
+        $background_image_id = 55;
+        $subtitle_text = 'Seamless and Efficient Processes Tailored for You';
         break;
 
     default:
@@ -17,7 +21,7 @@ switch ($pagename) {
 ?>
 
 <div class="header-main-page w-full bg-cover bg-center bg-no-repeat p-6 text-white text-center h-[500px]"
-    style="background-image: url('<?php echo esc_url(wp_get_attachment_url(50)); ?>');">
+    style="background-image: url('<?php echo esc_url(wp_get_attachment_url($background_image_id)); ?>');">
     <?php get_template_part('template_parts/header/menu', 'content') ?>
     <div class="header-main-page-title-subtitle flex flex-col justify-center items-center mt-10">
         <h1 class="title text-4xl font-bold mb-4"><?php the_title(); ?></h1>
