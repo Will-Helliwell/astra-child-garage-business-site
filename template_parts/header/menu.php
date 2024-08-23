6b4ceb2478a8770aeda9049d71c825b63d0de290
Will-Helliwell <wj.helliwell@gmail.com>
2024-08-23 11:05:15 +0800

    Formatting of desktop header menu/logo/buttons partially complete.

--- template_parts/header/menu.php
+++ template_parts/header/menu.php
@@ -1,16 +1,22 @@
-<div class="header-main-page-top-row flex justify-between items-center">
-    <div class="site-logo flex-1">
-        <?php if (has_custom_logo()) {
-            the_custom_logo();
-        } else { ?>
-            <a href="<?php echo home_url(); ?>" class="text-2xl font-bold"><?php bloginfo('name'); ?></a>
-        <?php } ?>
-    </div>
-    <nav class="menu-primary-custom flex-2">
-        <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'flex space-x-4')); ?>
-    </nav>
-    <div class="header-homepage-buttons flex-2 flex justify-around">
-        <a href="#" class="header-homepage-button bg-black text-white py-2 px-4 rounded hover:bg-gray-700">Button 1</a>
-        <a href="#" class="header-homepage-button bg-black text-white py-2 px-4 rounded hover:bg-gray-700">Button 2</a>
+<div class="flex justify-center">
+    <div class="header-main-page-top-row flex justify-between items-center w-full max-w-6xl px-4">
+        <div class="site-logo flex">
+            <?php if (has_custom_logo()) {
+                the_custom_logo();
+            } else { ?>
+                <a href="<?php echo home_url(); ?>" class="text-2xl font-bold"><?php bloginfo('name'); ?></a>
+            <?php } ?>
+        </div>
+        <nav class="menu-primary-custom flex flex-1 justify-center">
+            <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'flex space-x-8 text-xl font-normal')); ?>
+        </nav>
+        <div class="header-homepage-buttons flex space-x-4">
+            <a href="#" class="header-homepage-button flex items-center justify-center w-36 bg-transparent border border-pink-500 text-pink-500 py-2 px-8 rounded text-xs">
+                First Line<br>Second Line
+            </a>
+            <a href="#" class="header-homepage-button flex items-center justify-center w-36 bg-pink-500 text-white py-2 px-8 rounded hover:bg-pink-600">
+                Button 2
+            </a>
+        </div>
     </div>
 </div>
