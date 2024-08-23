38631500acc8b9fd8ddc2274203d6b95cef4a27e
Will-Helliwell <wj.helliwell@gmail.com>
2024-08-23 12:09:10 +0800

    Homepage title mobile responsive.

--- template_parts/header/header_homepage.php
+++ template_parts/header/header_homepage.php
@@ -2,12 +2,12 @@
      style="background-image: url('<?php echo esc_url(wp_get_attachment_url(24)); ?>');">
     <?php get_template_part('template_parts/header/menu', 'content') ?>
     <div class="bottom-row mt-28">
-        <h1 class="title text-4xl text-white font-bold">Grand Slam Garage Sales - Your</h1>
-        <h1 class="title text-4xl text-white font-bold mt-2">Trusted Partner in LA and OC</h1>
-        <h2 class="subtitle text-2xl text-homepage-subtitle-yellow font-semibold mt-2 mb-6">Helping you sell and remove items with ease!</h2>
+        <h1 class="title text-4xl text-white font-bold max-w-2xl mx-auto">
+            Grand Slam Garage Sales - Your Trusted Partner in LA and OC
+        </h1>
+        <h2 class="subtitle text-2xl text-homepage-subtitle-yellow font-semibold mt-2 mb-6 max-w-3xl mx-auto">
+            Helping you sell and remove items with ease!
+        </h2>
         <a href="#" class="cta-button bg-cta-pink text-white text-lg font-semibold py-3 px-6 rounded-lg hover:bg-pink-600">Get a Quote Today!</a>
-        <!-- <a href="#" class="header-homepage-button flex items-center justify-center w-40 bg-cta-pink text-white text-lg font-semibold py-0 px-6 rounded-lg hover:bg-pink-600">
-                Get In Touch
-            </a> -->
     </div>
 </div>
