bf1e850e0b1bed86133973d6e3bd57266aebd213
Will-Helliwell <wj.helliwell@gmail.com>
2024-08-23 09:53:15 +0800

    Title and subtitle typogaphies correct for main pages.

--- template_parts/header/header_main_page.php
+++ template_parts/header/header_main_page.php
@@ -2,20 +2,24 @@
 switch ($pagename) {
     case 'services':
         $background_image_id = 54;
-        $subtitle_text = 'Comprehensive Solutions for Your Needs in Los Angeles, Orange County and Nationwide';
+        $subtitle_line_one = 'Comprehensive Solutions for Your Needs in Los';
+        $subtitle_line_two = 'Angeles, Orange County and Nationwide';
         break;
     case 'who-we-are':
         $background_image_id = 50;
-        $subtitle_text = 'Discover The Story Behind Grand Slam Garage Sales';
+        $subtitle_line_one = 'Discover The Story Behind Grand Slam Garage Sales';
+        $subtitle_line_two = '';
         break;
     case 'how-it-works':
         $background_image_id = 55;
-        $subtitle_text = 'Seamless and Efficient Processes Tailored for You';
+        $subtitle_line_one = 'Seamless and Efficient Processes Tailored for You';
+        $subtitle_line_two = '';
         break;
 
     default:
         $background_image_id = 24;
-        $subtitle_text = 'Discover The Story Behind Grand Slam Garage Sales';
+        $subtitle_line_one = 'Discover The Story Behind Grand Slam Garage Sales';
+        $subtitle_line_two = '';
         break;
 }
 ?>
@@ -26,9 +30,10 @@ switch ($pagename) {
     <div class="absolute inset-0 bg-gradient-to-b from-header-blue/100 via-header-blue/70 to-transparent z-0"></div>
     <div class="relative z-10">
         <?php get_template_part('template_parts/header/menu', 'content') ?>
-        <div class="header-main-page-title-subtitle flex flex-col justify-center items-center mt-10">
-            <h1 class="title text-4xl font-bold mb-4"><?php the_title(); ?></h1>
-            <h2 class="subtitle text-2xl mb-4"><?php echo esc_html($subtitle_text); ?></h2>
+        <div class="header-main-page-title-subtitle flex flex-col justify-center items-center mt-28">
+            <h1 class="title text-5xl text-white font-bold mb-4"><?php the_title(); ?></h1>
+            <h2 class="subtitle text-3xl text-white mb-4"><?php echo esc_html($subtitle_line_one); ?></h2>
+            <h2 class="subtitle text-3xl text-white mb-4"><?php echo esc_html($subtitle_line_two); ?></h2>
         </div>
     </div>
 </div>
