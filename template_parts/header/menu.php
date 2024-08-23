959a4164c0954002ec611c89b09fc10554372962
Will-Helliwell <wj.helliwell@gmail.com>
2024-08-23 12:16:39 +0800

    Hide menu and buttons on mobile.

--- template_parts/header/menu.php
+++ template_parts/header/menu.php
@@ -7,10 +7,10 @@
                 <a href="<?php echo home_url(); ?>" class="text-2xl font-bold"><?php bloginfo('name'); ?></a>
             <?php } ?>
         </div>
-        <nav class="menu-primary-custom flex flex-1 justify-center">
+        <nav class="menu-primary-custom hidden md:flex flex-1 justify-center">
             <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'flex space-x-8 text-xl font-normal')); ?>
         </nav>
-        <div class="header-homepage-buttons flex space-x-4">
+        <div class="header-homepage-buttons hidden md:flex space-x-4">
             <a href="#" class="header-homepage-button flex flex-col items-center justify-center w-40 bg-transparent border border-cta-pink text-white text-xxs tracking-widest py-1 px-2 rounded-lg">
                 <p class="font-normal text-center w-full leading-tight">Call or Text</p>
                 <p class="font-bold text-center w-full leading-tight">949-876-3355</p>
