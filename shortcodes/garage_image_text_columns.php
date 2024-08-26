2b6ee0ba99b26dbb04cf5bad8d9f564eb46bd9fc
Will-Helliwell <wj.helliwell@gmail.com>
2024-08-26 11:35:15 +0800

    Styling correct on desktop.

--- shortcodes/garage_image_text_columns.php
+++ shortcodes/garage_image_text_columns.php
@@ -1,4 +1,5 @@
-<?php
+<?php 
+
 function garage_image_text_columns($atts)
 {
     // Define the default attributes
@@ -26,11 +27,16 @@ function garage_image_text_columns($atts)
     $text_content = wp_kses($atts['text'], $allowed_html);
     $subheading_content = wp_kses($atts['subheading'], $allowed_html);
 
-    // Determine the order of the columns
+    // Define padding and margin values
+    $outer_padding = 'p-28';  // Padding around the outer container
+    $inner_margin = 'mx-8';   // Margin between columns
+
+    // Define columns content
     $text_column = "
-        <div class='w-full lg:w-1/2 p-36 flex flex-col justify-center items-start'>
-            <h2 class='text-4xl font-semibold mb-4 font-poppins'>{$subheading_content}</h2>
-            <p class='mb-5 text-xl font-mona leading-relaxed'>{$text_content}</p>";
+        <div class='w-full lg:w-1/2 flex items-center'>
+            <div class='p-0 flex flex-col justify-center items-start h-full pr-8'>
+                <h2 class='text-4xl font-semibold mb-4 font-poppins'>{$subheading_content}</h2>
+                <p class='mb-5 text-xl font-mona leading-relaxed'>{$text_content}</p>";
 
     if (!empty($atts['button_text']) && !empty($atts['button_url'])) {
         $text_column .= "
@@ -39,27 +45,26 @@ function garage_image_text_columns($atts)
             </a>";
     }
 
-    $text_column .= "</div>";
+    $text_column .= "</div></div>";
 
     $image_column = "
-        <div class='w-full lg:w-1/2 p-36'>
-            <img src='{$atts['image_url']}' alt='' class='w-full h-auto rounded-3xl ring-2 ring-image-ring-blue'>
+        <div class='w-full lg:w-1/2'>
+            <div class='p-8'>
+                <img src='{$atts['image_url']}' alt='' class='w-full h-auto rounded-3xl ring-2 ring-image-ring-blue'>
+            </div>
         </div>";
 
-    // Combine columns based on the order attribute
-    if ($atts['order'] === 'left') {
-        $content = "
-        <div class='flex flex-col lg:flex-row'>
-            $text_column
-            $image_column
+    // Combine columns in an inner container
+    $inner_container = "
+        <div class='flex flex-col lg:flex-row $inner_margin'>
+            " . ($atts['order'] === 'left' ? "$text_column$image_column" : "$image_column$text_column") . "
         </div>";
-    } else {
-        $content = "
-        <div class='flex flex-col lg:flex-row'>
-            $image_column
-            $text_column
-        </div>";
-    }
+
+    // Wrap the inner container with the outer container
+    $content = "
+    <div class='$outer_padding'>
+        $inner_container
+    </div>";
 
     return $content;
 }
