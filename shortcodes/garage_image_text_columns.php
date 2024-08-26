337390c9d04bd139c402de335e54e47eda7ef58f
Will-Helliwell <wj.helliwell@gmail.com>
2024-08-26 10:44:47 +0800

    Allow html to be passed into shortcode.

--- shortcodes/garage_image_text_columns.php
+++ shortcodes/garage_image_text_columns.php
@@ -11,11 +11,26 @@ function garage_image_text_columns($atts)
         'image_url' => '',
     ), $atts, 'image_text_columns');
 
+    // Sanitize and allow specific HTML tags
+    $allowed_html = array(
+        'strong' => array(),
+        'b' => array(),
+        'em' => array(),
+        'i' => array(),
+        'a' => array(
+            'href' => array(),
+            'title' => array(),
+        ),
+        'br' => array(),
+    );
+    $text_content = wp_kses($atts['text'], $allowed_html);
+    $subheading_content = wp_kses($atts['subheading'], $allowed_html);
+
     // Determine the order of the columns
     $text_column = "
         <div class='w-full lg:w-1/2 p-36 flex flex-col justify-center items-start'>
-            <h2 class='text-3xl font-semibold mb-4 font-poppins'>{$atts['subheading']}</h2>
-            <p class='mb-4 text-xl font-mona'>{$atts['text']}</p>";
+            <h2 class='text-3xl font-semibold mb-4 font-poppins'>{$subheading_content}</h2>
+            <p class='mb-4 text-xl font-mona'>{$text_content}</p>";
 
     if (!empty($atts['button_text']) && !empty($atts['button_url'])) {
         $text_column .= "
