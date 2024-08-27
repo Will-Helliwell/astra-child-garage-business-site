e7c6e67f67cbd6b74bfb13d63a3339da7b37b8d6
Will-Helliwell <wj.helliwell@gmail.com>
2024-08-27 09:34:27 +0800

    Main footer works on desktop.

--- template_parts/footer.php
+++ template_parts/footer.php
@@ -1,8 +1,25 @@
-<div class="bg-black">
-    <div class="pb-10">
-        <p>section 1</p>
+<div class="footer-background bg-black">
+    <div class="columns-container pt-10 px-20 pb-10 text-white">
+        <div class="grid grid-cols-2 gap-0">
+            <div class="flex flex-col justify-between">
+                <a href="/" class="mb-4 underline">Home</a>
+                <a href="/who-we-are" class="mb-4 underline">Who We Are</a>
+                <a href="/services" class="mb-4 underline">Services</a>
+                <a href="/how-it-works" class="mb-4 underline">How it Works</a>
+                <a href="/faqs" class="mb-4 underline">FAQs</a>
+                <a href="/get-in-touch" class="underline">Get in Touch</a>
+            </div>
+            <div class="grid grid-cols-[3fr_1fr] gap-0">
+                <div></div>
+                <div class="flex flex-col justify-between">
+                    <p><b>Phone:</b> 949-876-3355</p>
+                    <p><b>Email:</b> help@grandslamgaragesales.com</p>
+                    <p><b>Business Hours:</b> Mon-Fri, 9am-6pm</p>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="flex justify-center pb-10">
+    <div class="social-icons-container flex justify-center pb-10">
         <?php
         set_query_var('social_icons_fill_color', 'black');
         set_query_var('social_icons_outer_color', 'white');
@@ -10,7 +27,7 @@
         ?>
     </div>
     <div class="border-t border-footer-text-grey w-full pb-10"></div>
-    <div class="text-center pb-10 text-footer-text-grey">
+    <div class="copyright-container text-center pb-10 text-footer-text-grey">
         <p>@2024 Grand Slam Garage Sales. All rights reserved.</p>
     </div>
 </div>
